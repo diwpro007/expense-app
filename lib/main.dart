102cf706d3ee14a08b94f50e25b5ce4dc607ae71
diwpro007 <diwserala@gmail.com>
2020-06-14 16:42:52 +0545

    Split the main file into multiple widget files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
+import 'package:expense_app/models/user_transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-
-import 'models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,21 +14,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: 't2',
-        title: 'Weekly Groceries',
-        amount: 16.53,
-        date: DateTime.now())
-  ];
-
-//  String titleInput;
-//  String amountInput;
-
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -49,74 +32,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Title'),
-                    controller: titleController,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Amount'),
-                    controller: amountController,
-                  ),
-                  FlatButton(
-                    child: Text('Add Transaction'),
-                    textColor: Colors.purple,
-                    onPressed: () {},
-                  )
-                ],
-              ),
-            ),
-          ),
-          Column(
-              children: transactions
-                  .map((tx) => Card(
-                        child: Row(
-                          children: <Widget>[
-                            Container(
-                              margin: EdgeInsets.symmetric(
-                                  vertical: 10, horizontal: 15),
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Colors.purple,
-                                  width: 2,
-                                ),
-                              ),
-                              padding: EdgeInsets.all(10),
-                              child: Text(
-                                '\$${tx.amount}',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                  color: Colors.purple,
-                                ),
-                              ),
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  tx.title,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                Text(
-                                  DateFormat('MMM dd, yyyy').format(tx.date),
-                                  style: TextStyle(color: Colors.grey),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      ))
-                  .toList()),
+          UserTransactions()
         ],
       ),
     );
---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,60 @@
+import 'package:expense_app/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget {
+  List<Transaction> transactionList;
+
+  TransactionList({
+    @required this.transactionList
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: transactionList
+            .map((tx) =>
+            Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin:
+                    EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '\$${tx.amount}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        DateFormat('MMM dd, yyyy').format(tx.date),
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ))
+            .toList());
+  }
+}
