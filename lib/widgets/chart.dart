72f6465393d7bfe3ae56233af512db912a42a1ab
Diwakar Serala <diwakar.serala@pdc01.lisnepal.com.np>
2020-06-25 15:40:43 +0545

    Added char bars

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,4 +1,5 @@
 import 'package:expense_app/models/transaction.dart';
+import 'package:expense_app/widgets/chart_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -11,19 +12,28 @@ class Chart extends StatelessWidget {
       final weekDay = DateTime.now().subtract(
         Duration(days: index),
       );
-      var totalSum = 0.0; 
+      var totalSum = 0.0;
       for (var i = 0; i < recentTransactions.length; i++) {
         if (recentTransactions[i].date.day == weekDay.day &&
             recentTransactions[i].date.month == weekDay.month &&
             recentTransactions[i].date.year == weekDay.year) {
-              totalSum += recentTransactions[i].amount;
-            }
+          totalSum += recentTransactions[i].amount;
+        }
       }
 
       print(DateFormat.E().format(weekDay));
       print(totalSum);
 
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
     });
   }
 
@@ -34,7 +44,10 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[],
+        children: groupedTransactionValues.map((data) {
+          return ChartBar(data['day'], data['amount'],
+            totalSpending == 0.0 ? 0.0 : (data['amount'] as double) / totalSpending);
+        }).toList(),
       ),
     );
   }
